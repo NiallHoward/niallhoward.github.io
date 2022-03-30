5d89dda62472ad72bc864579ab56c6236dbc2a02
NiallHoward <Nialljhoward@hotmail.com>
2022-03-30 19:44:51 +0100

    Animate in

--- js/js.js
+++ js/js.js
@@ -14,6 +14,26 @@ $(document).ready(function(){
         });
     });
 
+    // $(document).mousemove(function(event) {
+    //   windowWidth = $(window).width();
+    //   windowHeight = $(window).height();
+      
+    //   mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    //   mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+      
+    //   $('.content-wrap').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
+    // });
+
+    document.querySelector('.hovereffect').onmousemove = (e) => {
+
+      const x = e.pageX - e.target.offsetLeft
+      const y = e.pageY - e.target.offsetTop
+    
+      e.target.style.setProperty('--x', `${ x }px`)
+      e.target.style.setProperty('--y', `${ y }px`)
+      
+    }
+
     $( '.allclickable' ).click( function() {
         var clickLinkClass = $( this ).find( 'a.clicklink' );
 
