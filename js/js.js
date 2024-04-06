dc034d2e3861c230d37dea49e731255f45ae2993
NiallHoward <Nialljhoward@hotmail.com>
2024-04-06 19:18:18 +0100

    Animation-delay change

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  
     $(function() {
         //caches a jQuery object containing the header element
         var header = $(".buttonnavigation");
@@ -13,26 +14,6 @@ $(document).ready(function(){
             }
         });
     });
-
-    // $(document).mousemove(function(event) {
-    //   windowWidth = $(window).width();
-    //   windowHeight = $(window).height();
-      
-    //   mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
-    //   mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-      
-    //   $('.content-wrap').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #3498db, #9b59b6)');
-    // });
-
-    document.querySelector('.hovereffect').onmousemove = (e) => {
-
-      const x = e.pageX - e.target.offsetLeft
-      const y = e.pageY - e.target.offsetTop
-    
-      e.target.style.setProperty('--x', `${ x }px`)
-      e.target.style.setProperty('--y', `${ y }px`)
-      
-    }
 
     $( '.allclickable' ).click( function() {
         var clickLinkClass = $( this ).find( 'a.clicklink' );
