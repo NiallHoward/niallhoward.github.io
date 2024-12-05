ee68f28a0dca325f73bc660682a4317b2d9943cb
NiallHoward <Nialljhoward@hotmail.com>
2024-12-05 17:18:08 +0000

    Remove firebase

--- js/js.js
+++ js/js.js
@@ -1,34 +1,5 @@
 $(document).ready(function(){
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDfWuHzifBxtZiDG_kZL_B9W8NI-velFtg",
-    authDomain: "niallhoward-80986.firebaseapp.com",
-    projectId: "niallhoward-80986",
-  };
-  
-  // Initialize Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-
-  auth.createUserWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    var user = userCredential.user;
-    console.log("User created:", user);
-  })
-  .catch((error) => {
-    console.error("Error:", error.message);
-  });
-
-// Sign-in
-auth.signInWithEmailAndPassword(email, password)
-  .then((userCredential) => {
-    var user = userCredential.user;
-    console.log("Logged in:", user);
-  })
-  .catch((error) => {
-    console.error("Error:", error.message);
-  });
-
   $(function() {
         //caches a jQuery object containing the header element
         var header = $(".buttonnavigation");
